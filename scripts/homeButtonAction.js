fb1ab1f49ec61710dedebe421447b45d115e1f88
Marmax73 <almafuertebravo@hotmail.com>
2023-02-13 17:35:07 -0300

    creación de branch desarrollo

---
+++ scripts/homeButtonAction.js
@@ -0,0 +1,16 @@
+const d = document
+
+export default function boton(boton) {
+    d.addEventListener('click', e => {
+       if(e.target.matches(boton)){
+        // document.body.style.background = "#f3f7f5"
+        let div = document.createElement('div')
+        div.innerHTML = `<h3>Desde el div</h3>`
+   
+        div.style.background = "yellow"
+        div.style.width = "80%"
+        div.style.border = "solid dark-gray"
+        document.body.insertAdjacentElement('beforeend',div)
+       }
+    })
+   }
---
+++ scripts/homePresentation.js
@@ -0,0 +1,19 @@
+
+
+export default function Home(){
+const bienvenida = document.querySelector('#bienvenida')
+        // const homeContainer = d.createElement('div')
+        bienvenida.innerHTML = `<h1>Currículum Vitae</h1>
+        
+        <h2>Hola! Bienvenid@ a tu currículum</h2>
+        
+        <h3>Con sólo un click, puedes comenzar a crear tu CV personalizado</h3>
+        <br>
+        <h4>Adelante! Click al botón!</h4>
+        <br>
+        <button>Crear CV</button>
+        `
+    
+    // document.body.appendChild(homeContainer)
+}
+
