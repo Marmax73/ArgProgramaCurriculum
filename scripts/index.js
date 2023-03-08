ef7376787b4047d52561d0c26f80e1ecba482121
Marmax73 <almafuertebravo@hotmail.com>
2023-03-08 18:26:39 -0300

    función del botón. establecida. revizar

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,32 @@
-import boton from "./homeButtonAction.js"
-import Home from './homePresentation.js'
-
-const d = document
 
 
 
-d.addEventListener('DOMContentLoaded', e => {
-   Home()
-   boton('button')
+const boton = document.getElementById('boton--collapse')
+const listShow = document.getElementsByClassName('list__show')
 
-})
+
+    boton.onclick = () =>{
+      boton.classList.add('colorBg')
+      let heigth = 0;
+      let menu = boton.nextElementSibling
+      if(menu.clientHeight == '0'){
+          heigth=menu.scrollHeight      
+      }
+
+      let styles = `${heigth}`
+      menu.toggleAttribute(styles)
+      
+    }
+
+// const botonToggle = function(){
+//     let heigth = 0;
+//     let menu = boton.nextElementSibling
+//     if(menu.clientHeight == '0'){
+//         heigth=menu.scrollHeight      
+//     }
+//     menu.style.heigth = `${heigth}px`;
+// }
+
+// boton.addEventListener('click', function(){
+//      botonToggle()
+// })
